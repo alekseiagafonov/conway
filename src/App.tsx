e223c3965631b02a14f6b46265526affa77d6cf7
alekseiagafonov <aleksei.agafonov.2605@gmail.com>
2022-10-02 16:37:02 +0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,156 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC, useState, useRef, useCallback } from "react";
+import { Pause, Play, XCircle, Globe } from "react-feather";
+import "./App.css";
+import useInterval from "./useInterval";
 
-function App() {
+const numRows = 25;
+const numCols = 35;
+
+// Directions: N, S, E, W, NE, NW, SE, SW
+const operations = [
+  [0, 1], // right
+  [0, -1], // left
+  [1, -1], // top left
+  [-1, 1], // top right
+  [1, 1], // top
+  [-1, -1], // bottom
+  [1, 0], // bottom right
+  [-1, 0], // bottom left
+];
+
+const generateEmptyGrid = (): number[][] => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+  return rows;
+};
+
+const randomTiles = (): number[][] => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0)));
+  }
+  return rows;
+};
+
+const App: FC = () => {
+  const [grid, setGrid] = useState(() => {
+    return randomTiles();
+  });
+
+  const [running, setRunning] = useState(false);
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback((grid: number[][]) => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    let gridCopy = JSON.parse(JSON.stringify(grid));
+    for (let i = 0; i < numRows; i++) {
+      for (let j = 0; j < numCols; j++) {
+        let neighbors = 0;
+
+        operations.forEach(([x, y]) => {
+          const newI = i + x;
+          const newJ = j + y;
+
+          if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+            neighbors += grid[newI][newJ];
+          }
+        });
+
+        if (neighbors < 2 || neighbors > 3) {
+          gridCopy[i][j] = 0;
+        } else if (grid[i][j] === 0 && neighbors === 3) {
+          gridCopy[i][j] = 1;
+        }
+      }
+    }
+
+    setGrid(gridCopy);
+  }, []);
+
+  useInterval(() => {
+    runSimulation(grid);
+  }, 150);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1 className="flex justify-center text-3xl mb-6">Game of Life</h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols}, 20px)`,
+          width: "fit-content",
+          margin: "0 auto",
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                // Deep clone grid
+                let newGrid = JSON.parse(JSON.stringify(grid));
+                newGrid[i][k] = grid[i][k] ? 0 : 1;
+                setGrid(newGrid);
+              }}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][k] ? "green" : undefined,
+                border: "1px solid black",
+              }}
+            ></div>
+          ))
+        )}
+      </div>
+
+      <div className="flex justify-center mt-7">
+        <button
+          className="mr-1"
+          onClick={() => {
+            setRunning(!running);
+            if (!running) {
+              runningRef.current = true;
+            }
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <span className="flex justify-center ">
+            {running ? <Pause /> : <Play />}
+          </span>
+          <span>{running ? "Stop" : "Start"}</span>
+        </button>
+
+        <button
+          className="mr-1"
+          onClick={() => {
+            setGrid(randomTiles());
+          }}
+        >
+          <span className="flex justify-center ">
+            <Globe />
+          </span>
+          <span>Random</span>
+        </button>
+
+        <button
+          className="mr-1"
+          onClick={() => {
+            setGrid(generateEmptyGrid());
+          }}
+        >
+          <span className="flex justify-center ">
+            <XCircle />
+          </span>
+          <span>Clear</span>
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
